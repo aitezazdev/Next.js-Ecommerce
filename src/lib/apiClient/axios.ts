02f549a44e19399fb2b26365d504e9f3494a8ec2
aitezazdev <aitezazsikandar@gmail.com>
2026-06-11 23:26:43 +0500

    fix: redirect to login page on expired JWT (401 status)

--- src/lib/apiClient/axios.ts
+++ src/lib/apiClient/axios.ts
@@ -8,4 +8,16 @@ const api = axios.create({
   },
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      if (typeof window !== "undefined") {
+        window.location.href = "/auth/signin";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
